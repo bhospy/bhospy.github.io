f5fee845397dc8a9da431e54493ee5546f241c75
Javid Shiriyev <shiriyevcavid@gmail.com>
2022-02-09 15:59:49 +0400

    should be working

---
+++ static/js/views/blogs.js
@@ -0,0 +1,98 @@
+import abstract from "./abstract.js";
+
+export default class extends abstract {
+	constructor() {
+		super();
+		this.setTitle("Home");
+	}
+
+	async getHtml() {
+		return `
+			<nav id="sidecontent">
+				<ul class="sidebar">
+				  <li><a href="#" onclick="changecanvas('Home1')">Home 1</a></li>
+				  <li><a href="#" onclick="changecanvas('Home2')">Home 2</a></li>
+				  <li><a href="#" onclick="changecanvas('Home3')">Home 3</a></li>
+				  <li><a href="#" onclick="changecanvas('Home4')">Home 4</a></li>
+				  <li><a href="#" onclick="changecanvas('Home5')">Home 5</a></li>
+				</ul>
+			</nav>
+
+			<div id="canvascontent" class="canvas">
+				<p>I was updated</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		`;
+	}
+}
--- static/js/views/people.js
+++ static/js/views/people.js
@@ -1,65 +1,79 @@
-<nav id="sidecontent">
-  <ul class="sidebar">
-    <li><a href="#" onclick="clickchange(1)">Javid Shiriyev</a></li>
-  </ul>
-</nav>
+import abstract from "./abstract.js";
 
-<div id="canvascontent" class="canvas">
-  <p class="title"><img src="..\photos\headshots\p2.jpg" alt="Javid's photo">Javid Shiriyev, Ph.D.</p>
-  <p>My research revolves around studying fluid flow modeling through porous media, including the problems associated with uncertainty handling in data collection and model incorporation. I acquire the rudiments of mechanics and the basics of data science and have taught fluid and rock mechanics, reservoir characterization and petrophysics-related courses. Besides teaching, currently, I am also involved in the field development planning for Gum-Daniz and Bahar fields.</p>
-  <p class="title">Education</p>
-  <ul>
-    <li>Ph.D., Petroleum and Geosystems Engineering, The University of Texas at Austin, 2018</li>
-    <li>M.Sc., Petroleum and Natural Gas Engineering, Middle East Technical University, 2014</li>
-    <li>B.Sc., Petroleum and Natural Gas Engineering, Middle East Technical University, 2012</li>
-  </ul>
-  <p class="title">Experience</p>
-  <ul>
-    <li>Reservoir Simulation Engineer, Bahar Energy Operated Company, Jun. 2021 to present</li>
-    <li>Senior Lecturer, Baku Higher Oil School, Nov. 2020 to present</li>
-    <li>Visiting Assistant Professor, Middle East Technical University NCC, 2020</li>
-    <li>Postdoctoral Research Fellow, The University of Texas at Austin, 2019-2020</li>
-    <li>Reservoir Engineering Intern, Aramco Services Company, 2017</li>
-    <li>Petroleum Engineering Intern, BP, 2013</li>
-    <li>Drilling Engineering Intern, BP, 2011</li>
-    <li>Student Intern, TPAO, 2010</li>
-  </ul>
-  <p class="title">Research Interests</p>
-  <ul>
-    <li>Perforation and wellbore stability in unconsolidated sands</li>
-    <li>Sand control completions</li>
-    <li>Frac-packing poorly consolidated sands</li> 
-    <li>Hydraulic fracture propagation in soft formations</li>
-    <li>Fluid flow in fractured rock</li>
-  </ul>
-  <p class="title">Courses Taught</p>
-  <ul>
-    <li>Fluid Flow Through Porous Media</li>
-    <li>Applied Geostatistics for Reservoir Characterization</li>
-    <li>Formation Evaluation with Well Logging</li>
-    <li>Fundamentals of Petrophysics</li>
-    <li>Reservoir Fluid Properties</li>
-    <li>Reservoir Rock Mechanics</li>
-    <li>Scientific Computing with Python</li>
-    <li>Energy Resources</li>
-  </ul>
-  <p class="title">Honors</p>
-  <ul>
-    <li>SPWLA Annual Symposium Paper Contest Winner, 2017</li>
-    <li>Conoco-Phillips Scholarship recipient, 2014-15</li>
-    <li>METU Graduate Course Performance Award, 2013</li>
-    <li>BP Graduate Scholarship recipient, 2012-14</li>
-    <li>Chamber of Petroleum Engineers Award, Turkey, 2012</li>
-    <li>METU Undergraduate First Ranking Student Award, 2012</li>
-    <li>BP Undergraduate Scholarship recipient, 2008-12</li>
-  </ul>
-  <p class="title">Publications</p>
-  <ul>
-    <li>Zhang, P., J. Shiriyev, M.K. Sen, and M.M. Sharma, 2019, Fast inversion of downhole electrical measurements for proppant mapping using very fast simulated annealing: Geophysics, 85, no. 1, D13-D22, doi: 10.1190/GEO2018-0749.1.</li>
-    <li>Shiriyev, J., 2018, A tri-axial electromagnetic induction tool for hydraulic fracture diagnostics: Ph.D. Dissertation, The University of Texas at Austin, Austin, TX, USA.</li>
-    <li>Sharma, M.M., J. Shiriyev, P. Zhang, Y. Brick, D. Glowka, J. Gabelmann, and R. Houston, 2018, Fracture diagnostics using low frequency electromagnetic induction and electrically conductive proppants: Final Scientific Report, The University of Texas at Austin, Austin, TX, USA, DOE Award Number: DE-FE0024271.</li>
-    <li>Shiriyev, J., Y. Brick, P. Zhang, A.E. Yilmaz, C. Torres-Verdin, M.M. Sharma, T. Hosbach, M.A. Oerkfitz, and J. Gabelmann, 2018, Experiments and simulations of a prototype tri-axial electromagnetic induction logging tool for open-hole hydraulic fracture diagnostics: Geophysics, 83, no. 3, D73-D81, doi: 10.1190/GEO2017-0354.1.</li>
-    <li>Zhang, P., J. Shiriyev, C. Torres-Verdin, M.M. Sharma, Y. Brick, J. Massey, and A.E. Yilmaz, 2016, Fracture diagnostics using a low-frequency electromagnetic induction method: Presented at the 50th US Rock Mechanics/Geomechanics Symposium.</li> 
-    <li>Shiriyev, J., 2014, Discrete fracture network modeling in a carbon dioxide flooded heavy oil reservoir: M.S. Thesis, Middle East Technical University, Ankara, Turkey.</li>
-  </ul>
-</div>
+export default class extends abstract {
+  constructor() {
+    super();
+    this.setTitle("People");
+  }
+
+  async getHtml() {
+    return `
+      <nav id="sidecontent">
+        <ul class="sidebar">
+          <li><a href="#" onclick="clickchange(1)">Javid Shiriyev</a></li>
+        </ul>
+      </nav>
+
+      <div id="canvascontent" class="canvas">
+      <!--<img src="..\photos\headshots\p2.jpg" alt="Javid's photo">-->
+        <p class="title">Javid Shiriyev, Ph.D.</p>
+        <p>My research revolves around studying fluid flow modeling through porous media, including the problems associated with uncertainty handling in data collection and model incorporation. I acquire the rudiments of mechanics and the basics of data science and have taught fluid and rock mechanics, reservoir characterization and petrophysics-related courses. Besides teaching, currently, I am also involved in the field development planning for Gum-Daniz and Bahar fields.</p>
+        <p class="title">Education</p>
+        <ul>
+          <li>Ph.D., Petroleum and Geosystems Engineering, The University of Texas at Austin, 2018</li>
+          <li>M.Sc., Petroleum and Natural Gas Engineering, Middle East Technical University, 2014</li>
+          <li>B.Sc., Petroleum and Natural Gas Engineering, Middle East Technical University, 2012</li>
+        </ul>
+        <p class="title">Experience</p>
+        <ul>
+          <li>Reservoir Simulation Engineer, Bahar Energy Operated Company, Jun. 2021 to present</li>
+          <li>Senior Lecturer, Baku Higher Oil School, Nov. 2020 to present</li>
+          <li>Visiting Assistant Professor, Middle East Technical University NCC, 2020</li>
+          <li>Postdoctoral Research Fellow, The University of Texas at Austin, 2019-2020</li>
+          <li>Reservoir Engineering Intern, Aramco Services Company, 2017</li>
+          <li>Petroleum Engineering Intern, BP, 2013</li>
+          <li>Drilling Engineering Intern, BP, 2011</li>
+          <li>Student Intern, TPAO, 2010</li>
+        </ul>
+        <p class="title">Research Interests</p>
+        <ul>
+          <li>Perforation and wellbore stability in unconsolidated sands</li>
+          <li>Sand control completions</li>
+          <li>Frac-packing poorly consolidated sands</li> 
+          <li>Hydraulic fracture propagation in soft formations</li>
+          <li>Fluid flow in fractured rock</li>
+        </ul>
+        <p class="title">Courses Taught</p>
+        <ul>
+          <li>Fluid Flow Through Porous Media</li>
+          <li>Applied Geostatistics for Reservoir Characterization</li>
+          <li>Formation Evaluation with Well Logging</li>
+          <li>Fundamentals of Petrophysics</li>
+          <li>Reservoir Fluid Properties</li>
+          <li>Reservoir Rock Mechanics</li>
+          <li>Scientific Computing with Python</li>
+          <li>Energy Resources</li>
+        </ul>
+        <p class="title">Honors</p>
+        <ul>
+          <li>SPWLA Annual Symposium Paper Contest Winner, 2017</li>
+          <li>Conoco-Phillips Scholarship recipient, 2014-15</li>
+          <li>METU Graduate Course Performance Award, 2013</li>
+          <li>BP Graduate Scholarship recipient, 2012-14</li>
+          <li>Chamber of Petroleum Engineers Award, Turkey, 2012</li>
+          <li>METU Undergraduate First Ranking Student Award, 2012</li>
+          <li>BP Undergraduate Scholarship recipient, 2008-12</li>
+        </ul>
+        <p class="title">Publications</p>
+        <ul>
+          <li>Zhang, P., J. Shiriyev, M.K. Sen, and M.M. Sharma, 2019, Fast inversion of downhole electrical measurements for proppant mapping using very fast simulated annealing: Geophysics, 85, no. 1, D13-D22, doi: 10.1190/GEO2018-0749.1.</li>
+          <li>Shiriyev, J., 2018, A tri-axial electromagnetic induction tool for hydraulic fracture diagnostics: Ph.D. Dissertation, The University of Texas at Austin, Austin, TX, USA.</li>
+          <li>Sharma, M.M., J. Shiriyev, P. Zhang, Y. Brick, D. Glowka, J. Gabelmann, and R. Houston, 2018, Fracture diagnostics using low frequency electromagnetic induction and electrically conductive proppants: Final Scientific Report, The University of Texas at Austin, Austin, TX, USA, DOE Award Number: DE-FE0024271.</li>
+          <li>Shiriyev, J., Y. Brick, P. Zhang, A.E. Yilmaz, C. Torres-Verdin, M.M. Sharma, T. Hosbach, M.A. Oerkfitz, and J. Gabelmann, 2018, Experiments and simulations of a prototype tri-axial electromagnetic induction logging tool for open-hole hydraulic fracture diagnostics: Geophysics, 83, no. 3, D73-D81, doi: 10.1190/GEO2017-0354.1.</li>
+          <li>Zhang, P., J. Shiriyev, C. Torres-Verdin, M.M. Sharma, Y. Brick, J. Massey, and A.E. Yilmaz, 2016, Fracture diagnostics using a low-frequency electromagnetic induction method: Presented at the 50th US Rock Mechanics/Geomechanics Symposium.</li> 
+          <li>Shiriyev, J., 2014, Discrete fracture network modeling in a carbon dioxide flooded heavy oil reservoir: M.S. Thesis, Middle East Technical University, Ankara, Turkey.</li>
+        </ul>
+      </div>
+    `;
+  }
+}
