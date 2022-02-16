2abe9a107d8d16867e5e2d4f889fe30405275c23
Javid Shiriyev <jshiriyev@utexas.edu>
2022-02-16 22:55:45 +0400

    cleaned the css and js files

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
 var items = document.querySelectorAll('.canvas-item');
 
-for (let i = 0; i < items.length; i++) {
-  items[i].style.display = 'none'
-}
-
 items[0].style.display = 'block'
 
 function changeCanvasItem(ID) {
