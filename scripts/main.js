155735b56a6b4badb6e9231230e2ee5f246e968e
Javid Shiriyev <jshiriyev@utexas.edu>
2022-02-18 00:23:43 +0400

    trying to add accordion to navigation bar

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,18 @@ var items = document.querySelectorAll('.canvas-item');
 
 items[0].style.display = 'block'
 
+function navAccFunc(itemId) {
+	var x = document.getElementById(itemId);
+	if (x.className.indexOf("w3-show") == -1) {
+	  x.className += " w3-show";
+	  x.previousElementSibling.className += " w3-green";
+	} else { 
+	  x.className = x.className.replace(" w3-show", "");
+	  x.previousElementSibling.className = 
+	  x.previousElementSibling.className.replace(" w3-green", "");
+	}
+  }
+
 function changeCanvasItem(ID) {
 
 	for (let i = 0; i < items.length; i++) {
