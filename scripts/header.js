f26de0bf8360676d3c34a5638c925ff3b55b034d
Javid Shiriyev <jshiriyev@utexas.edu>
2022-02-19 16:48:38 +0400

    test update of mono header

--- scripts/header.js
+++ scripts/header.js
@@ -4,9 +4,9 @@ class PageHeader extends HTMLElement {
         <header>
             <h2>ENERGY RESOURCES - SCIENCE & ENGINEERING</h2>
             <select name="Language">
-                <option value="Azerbaijani">Azerbaijani</option>
-                <option value="Turkish">Turkish</option>
-                <option value="English">English</option>
+                <option value="Azerbaijani">English</option>
+                <option value="Turkish">Azerbaijani</option>
+                <option value="English">Turkish</option>
                 <option value="Russian">Russian</option>
             </select>
         </header>
