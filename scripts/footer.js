e491763991c148c1331d72bbaadb771f559c5dbf
Javid Shiriyev <jshiriyev@utexas.edu>
2022-02-23 19:38:06 +0400

    added auto date generating footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,9 +1,17 @@
+var today = new Date();
+
+var date = today.getDate();
+var month = today.toLocaleString('en-us', { month: 'short' });
+var year = today.getFullYear();
+
+footer = 'Last Edited ' + month + '-' + date + ', ' + year;
+
 class PageFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer>
             <ul class="infobar">
-                <li>Last Edited Feb-23, 2022</li>
+                <li>${footer}</li>
             </ul>
         </footer>
         `
