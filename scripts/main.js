d57426497283850f749a7baa9c03ae88734eee2f
Javid Shiriyev <shiriyevcavid@gmail.com>
2022-02-23 11:45:36 +0400

    working on responsive navbar, for now seems to be working

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
-var items = document.querySelectorAll('.canvas-item');
+const ribbonToggle = document.getElementsByClassName('ribbon-toggle')[0]
+const ribbonLinks = document.getElementsByClassName('ribbon-links')[0]
 
-items[0].style.display = 'block'
-
-function changeCanvasItem(ID) {
-
-	for (let i = 0; i < items.length; i++) {
-	  items[i].style.display = 'none'
-	}
-
-	items[ID].style.display = 'block'
-	
-}
+ribbonToggle.addEventListener('click', () => {
+  ribbonLinks.classList.toggle('active')
+})
 
 var acc = document.getElementsByClassName("sidebar-accordion");
 
@@ -34,4 +27,18 @@ for (let i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+
+var items = document.querySelectorAll('.canvas-item');
+
+items[0].style.display = 'block'
+
+function changeCanvasItem(ID) {
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.display = 'none'
+  }
+
+  items[ID].style.display = 'block'
+  
 }
