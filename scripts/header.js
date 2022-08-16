b04a2893480623a9a3dab8a87a2f0d0c309c2f47
Javid Shiriyev <shiriyevcavid@gmail.com>
2022-08-16 16:51:10 +0400

    working on to update webpage with recent changes

--- scripts/header.js
+++ scripts/header.js
@@ -2,13 +2,7 @@ class PageHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <h2>ENERGY RESOURCES - SCIENCE & ENGINEERING</h2>
-            <select name="Language">
-                <option value="English">English</option>
-                <option value="Azerbaijani">Azerbaijani</option>
-                <option value="Turkish">Turkish</option>
-                <option value="Russian">Russian</option>
-            </select>
+            <h2>FORMATION EVALUATION - ENGINEERING & MODELING</h2>
         </header>
         `
     }
